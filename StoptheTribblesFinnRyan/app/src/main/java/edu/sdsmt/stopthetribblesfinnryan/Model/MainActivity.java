3138e5b1e00cc4b0cc046ef6ba7cdef17881b789
Finn, Ryan <Ryan.Finn@mines.sdsmt.edu>
2022-04-11 11:09:03 -0600

    Practice layouts

--- StoptheTribblesFinnRyan/app/src/main/java/edu/sdsmt/stopthetribblesfinnryan/Model/MainActivity.java
+++ StoptheTribblesFinnRyan/app/src/main/java/edu/sdsmt/stopthetribblesfinnryan/Model/MainActivity.java
@@ -3,14 +3,31 @@ package edu.sdsmt.stopthetribblesfinnryan.Model;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import edu.sdsmt.stopthetribblesfinnryan.R;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView hunger, days;
+    private Button eat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hunger = findViewById(R.id.hunger);
+        days = findViewById(R.id.days);
+        eat = findViewById(R.id.eat);
+
+        String text = hunger.getText().toString() + "0";
+        hunger.setText(text);
+
+        text = days.getText().toString() + "1";
+        days.setText(text);
+
+        text = eat.getText().toString() + "3";
+        eat.setText(text);
     }
 }
