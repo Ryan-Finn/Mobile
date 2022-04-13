686742ad5ab1ce36dc3b0b1109e902d89861dcbc
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-04-13 14:30:24 -0600

    Fields

--- Tutoria7FinnRyan/app/src/main/java/edu/sdsmt/tutoria7finnryan/MainActivity.java
+++ Tutoria7FinnRyan/app/src/main/java/edu/sdsmt/tutoria7finnryan/MainActivity.java
@@ -2,9 +2,18 @@ package edu.sdsmt.tutoria7finnryan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager = null;
+
+    private double latitude = 0;
+    private double longitude = 0;
+    private boolean valid = false;
+    private double toLatitude = 0;
+    private double toLongitude = 0;
+    private String to = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
