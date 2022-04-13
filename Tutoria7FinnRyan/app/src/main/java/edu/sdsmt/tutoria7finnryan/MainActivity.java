28e69c0f56de11d12d3cb7df979e1b80aab48235
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-04-13 14:32:47 -0600

    Preferences

--- Tutoria7FinnRyan/app/src/main/java/edu/sdsmt/tutoria7finnryan/MainActivity.java
+++ Tutoria7FinnRyan/app/src/main/java/edu/sdsmt/tutoria7finnryan/MainActivity.java
@@ -2,12 +2,17 @@ package edu.sdsmt.tutoria7finnryan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager = null;
-
+    private SharedPreferences settings = null;
+    private final static String TO = "to";
+    private final static String TOLAT = "tolat";
+    private final static String TOLONG = "tolong";
     private double latitude = 0;
     private double longitude = 0;
     private boolean valid = false;
@@ -19,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        to = settings.getString(TO, "McLaury Building");
+        toLatitude = Double.parseDouble(settings.getString(TOLAT, "44.075104"));
+        toLongitude = Double.parseDouble(settings.getString(TOLONG, "-103.206819"));
     }
 }
