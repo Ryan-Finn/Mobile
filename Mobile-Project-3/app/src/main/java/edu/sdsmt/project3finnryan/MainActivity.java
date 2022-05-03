7ce5c0977f946f14a9d2c361c3d8c2b5ab450acb
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-05-02 23:56:53 -0600

    ...

--- Mobile-Project-3/app/src/main/java/edu/sdsmt/project3finnryan/MainActivity.java
+++ Mobile-Project-3/app/src/main/java/edu/sdsmt/project3finnryan/MainActivity.java
@@ -5,187 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Address;
 import android.location.Criteria;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.OrientationEventListener;
 import android.view.Surface;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private final ActiveListener activeListener = new ActiveListener();
-    private LocationManager locationManager;
-    private SharedPreferences settings;
-    private final static String TO = "to";
-    private final static String TOLAT = "tolat";
-    private final static String TOLONG = "tolong";
+    private MainView view;
+
     private double latitude, longitude = 0;
     private boolean valid = false;
-    private double toLatitude, toLongitude;
-    private String to;
-    private TextView viewTo, viewLatitude, viewLongitude, viewDistance, viewProvider;
-    private Location locStart, locEnd;
+    private TextView viewProvider, viewLatitude, viewLongitude;
     private static final int NEED_PERMISSIONS = 1;
-
-    private SensorManager sensorManager;
-    private AccelListener accelListener;
-    private Sensor accelSensor;
     private static int surfaceRotation;
+    private static float x, y, z = 0;
+
+    private AccelListener accelListener;
+    private ActiveListener activeListener;
     private OrientationEventListener orientationListener;
-    private static float x, y, z, ax, ay, az = 0;
 
-    private static class AccelListener implements SensorEventListener {
-        @Override
-        public void onAccuracyChanged(Sensor arg0, int arg1) {}
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            float a = 0.95f;
-            x = (1 - a) * event.values[0] + a * x;
-            y = (1 - a) * event.values[1] + a * y;
-            z = (1 - a) * event.values[2] + a * z;
-
-            switch(surfaceRotation) {
-                default:
-                    ax = x;
-                    ay = y;
-                    az = z;
-                    break;
-                case Surface.ROTATION_90:
-                    ax = -y;
-                    ay = x;
-                    az = z;
-                    break;
-                case Surface.ROTATION_180:
-                    ax = -x;
-                    ay = -y;
-                    az = z;
-                    break;
-                case Surface.ROTATION_270:
-                    ax = y;
-                    ay = -x;
-                    az = z;
-                    break;
-            }
-
-            Log.d("onSensorChanged", "" + ax + ", " + ay + ", " + az);
-        }
-    }
-
-    private void getOrientation() {
-        WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
-        surfaceRotation = wm.getDefaultDisplay().getRotation();
-    }
-
-    public void onResume() {
-        super.onResume();
-        orientationListener.enable();
-
-        sensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
-        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        if (accelSensor != null) {
-            accelListener = new AccelListener();
-            sensorManager.registerListener(accelListener, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-    }
-
-    public void onPause() {
-        super.onPause();
-        orientationListener.disable();
-
-        if (accelSensor != null) {
-            sensorManager.unregisterListener(accelListener);
-            accelListener = null;
-            accelSensor = null;
-        }
-    }
+    private Sensor accelSensor;
+    private SensorManager sensorManager;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+        view = this.findViewById(R.id.mainView);
 
-        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-
-        settings = PreferenceManager.getDefaultSharedPreferences(this);
-        to = settings.getString(TO, "McLaury Building");
-        toLatitude = Double.parseDouble(settings.getString(TOLAT, "44.075104"));
-        toLongitude = Double.parseDouble(settings.getString(TOLONG, "-103.206819"));
-
-        locStart = new Location("start");
-        locEnd = new Location("end");
-        locEnd.setLatitude(toLatitude);
-        locEnd.setLongitude(toLongitude);
-
-        viewTo = findViewById(R.id.textTo);
+        viewProvider = findViewById(R.id.textProvider);
         viewLatitude = findViewById(R.id.textLatitude);
         viewLongitude = findViewById(R.id.textLongitude);
-        viewDistance = findViewById(R.id.textDistance);
-        viewProvider = findViewById(R.id.textProvider);
 
-        orientationListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
+        activeListener = new ActiveListener();
+        orientationListener = new OrientationEventListener(this, SensorManager.SENSOR_DELAY_UI) {
             @Override
             public void onOrientationChanged(int i) {
                 getOrientation();
             }
         };
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.itemGrubby:
-                newTo(getString(R.string.grubby), 44.074834, -103.207562);
-                return true;
-
-            case R.id.itemHome:
-                newTo(getString(R.string.home), 38.928325398654174, -104.70940438833615);
-                return true;
-
-            case R.id.itemMcLaury:
-                newTo(getString(R.string.mclaury), 44.075104, -103.206819);
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
     }
 
     @Override
@@ -197,6 +73,32 @@ public class MainActivity extends AppCompatActivity {
 
         setUI();
         registerListeners();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        orientationListener.enable();
+
+        sensorManager = (SensorManager)this.getSystemService(Context.SENSOR_SERVICE);
+        accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (accelSensor != null) {
+            accelListener = new AccelListener();
+            sensorManager.registerListener(accelListener, accelSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        orientationListener.disable();
+
+        if (accelSensor != null) {
+            sensorManager.unregisterListener(accelListener);
+            accelListener = null;
+            accelSensor = null;
+        }
     }
 
     @Override
@@ -220,17 +122,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUI() {
-        viewTo.setText(to);
         viewLatitude.setText("");
         viewLongitude.setText("");
-        viewDistance.setText("");
 
         if (!valid)
             return;
 
         viewLatitude.setText(String.format(Locale.getDefault(),"%1$6.7fm", (float)latitude));
         viewLongitude.setText(String.format(Locale.getDefault(),"%1$6.7fm", (float)longitude));
-        viewDistance.setText(String.format(Locale.getDefault(),"%1$6.1fm", locStart.distanceTo(locEnd)));
+        view.setStartLocation((float)latitude, (float)longitude);
+        view.setLocation((float)latitude, (float)longitude);
+    }
+
+    private void getOrientation() {
+        WindowManager wm = (WindowManager)getSystemService(Context.WINDOW_SERVICE);
+        surfaceRotation = wm.getDefaultDisplay().getRotation();
     }
 
     private void registerListeners() {
@@ -267,75 +173,10 @@ public class MainActivity extends AppCompatActivity {
             return;
 
         valid = true;
-        locStart = location;
         latitude = location.getLatitude();
         longitude = location.getLongitude();
 
         setUI();
-    }
-
-    private void newTo(String address, double lat, double lon) {
-        to = address;
-        toLatitude = lat;
-        toLongitude = lon;
-        locEnd.setLatitude(lat);
-        locEnd.setLongitude(lon);
-
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString(TO, address);
-        editor.putString(TOLAT, "" + lat);
-        editor.putString(TOLONG, "" + lon);
-        editor.apply();
-
-        setUI();
-    }
-
-    public void onNew(View view) {
-        EditText location = findViewById(R.id.editLocation);
-        final String address = location.getText().toString().trim();
-        newAddress(address);
-    }
-
-    private void newAddress(final String address) {
-        if (address.equals(""))
-            return;
-
-        new Thread(() -> lookupAddress(address)).start();
-    }
-
-    private void lookupAddress(String address) {
-        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.US);
-        boolean exception = false;
-        List<Address> locations;
-
-        try {
-            locations = geocoder.getFromLocationName(address, 1);
-        } catch (IOException ex) {
-            locations = null;
-            exception = true;
-        }
-
-        boolean finalException = exception;
-        List<Address> finalLocations = locations;
-        this.runOnUiThread(() -> newLocation(address, finalException, finalLocations));
-    }
-
-    private void newLocation(String address, boolean exception, List<Address> locations) {
-        if (exception) {
-            Toast.makeText(MainActivity.this, R.string.exception, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if (locations == null || locations.size() == 0) {
-            Toast.makeText(this, R.string.couldnotfind, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        EditText location = findViewById(R.id.editLocation);
-        location.setText("");
-
-        Address a = locations.get(0);
-        newTo(address, a.getLatitude(), a.getLongitude());
     }
 
     private class ActiveListener implements LocationListener {
@@ -352,6 +193,45 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onProviderDisabled(@NonNull String provider) {
             registerListeners();
+        }
+    }
+
+    private class AccelListener implements SensorEventListener {
+        @Override
+        public void onAccuracyChanged(Sensor arg0, int arg1) {}
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float a = 0.95f;
+            float ax, ay, az;
+            x = (1 - a) * event.values[0] + a * x;
+            y = (1 - a) * event.values[1] + a * y;
+            z = (1 - a) * event.values[2] + a * z;
+
+            switch(surfaceRotation) {
+                default:
+                    ax = x;
+                    ay = y;
+                    az = z;
+                    break;
+                case Surface.ROTATION_90:
+                    ax = -y;
+                    ay = x;
+                    az = z;
+                    break;
+                case Surface.ROTATION_180:
+                    ax = -x;
+                    ay = -y;
+                    az = z;
+                    break;
+                case Surface.ROTATION_270:
+                    ax = y;
+                    ay = -x;
+                    az = z;
+                    break;
+            }
+
+            view.setScale(az / 10f);
         }
     }
 }
--- Mobile-Project-3/app/src/main/java/edu/sdsmt/project3finnryan/MainView.java
+++ Mobile-Project-3/app/src/main/java/edu/sdsmt/project3finnryan/MainView.java
@@ -1,25 +1,143 @@
 package edu.sdsmt.project3finnryan;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class MainView extends View {
-
+    private final static int factor = 1000000000;
+    private float startLat, startLong, latitude, longitude = 0;
+    private Paint fillPaint, outlinePaint, penPaint;
+    private Bitmap canvasBitmap = null;
+    private boolean newLoc = true;
+    private float x, y, scale, prevScale = -1;
 
     public MainView(Context context) {
         super(context);
+        init();
     }
 
     public MainView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public MainView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
+    private void init() {
+        scale = 1;
 
+        fillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        fillPaint.setColor(Color.WHITE);
+
+        outlinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        outlinePaint.setStyle(Paint.Style.STROKE);
+        outlinePaint.setStrokeWidth(5.0f);
+        outlinePaint.setColor(Color.BLACK);
+
+        penPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        penPaint.setStrokeWidth(10.0f);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (canvasBitmap == null)
+            canvasBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+
+        float prevX = x;
+        float prevY = y;
+        if (x == -1) {
+            prevX = factor * (startLat - latitude) / getWidth() + getWidth() / 2f;
+            prevY = factor * (startLong - longitude) / getWidth() + getHeight() / 2f;
+        }
+
+        x = factor * (startLat - latitude) / getWidth() + getWidth() / 2f;
+        y = factor * (startLong - longitude) / getWidth() + getHeight() / 2f;
+
+        canvas.drawRect(0, 0, getWidth(), getHeight(), fillPaint);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), outlinePaint);
+
+        if (newLoc && scale >= 0.45f)
+            makeBitmap(prevX, prevY);
+        canvas.drawBitmap(canvasBitmap, 0, 0, null);
+
+        canvas.save();
+        canvas.translate(x, y);
+        canvas.drawCircle(0, 0, scale * 50f, penPaint);
+        canvas.restore();
+
+        newLoc = false;
+    }
+
+    private void makeBitmap(float prevX, float prevY) {
+        int sz = 16;
+        float X, Y, R;
+        int w = canvasBitmap.getWidth();
+        int h = canvasBitmap.getHeight();
+        int[] mapDest = new int[w * h];
+
+        canvasBitmap.getPixels(mapDest, 0, w, 0, 0, w, h);
+
+        for (int k = 0; k <= sz; k++) {
+            X = ((sz - k) * prevX + k * x) / sz;
+            Y = ((sz - k) * prevY + k * y) / sz;
+            R = 50f * ((sz - k) * prevScale + k * scale) / sz;
+
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    if ((i - X) * (i - X) + (j - Y) * (j - Y) <= R * R)
+                        mapDest[j * w + i] = Color.BLACK;
+        }
+
+        canvasBitmap = Bitmap.createBitmap(mapDest, w, h, Bitmap.Config.ARGB_8888);
+    }
+
+    public void setScale(float scale) {
+        penPaint.setStyle(Paint.Style.STROKE);
+        penPaint.setColor(Color.GRAY);
+        this.scale = scale;
+
+        if (scale < 0.25f)
+            this.scale = 0.25f;
+        else if (scale >= 0.95f)
+            this.scale = 1;
+
+        if (scale >= 0.45f) {
+            penPaint.setStyle(Paint.Style.FILL);
+            penPaint.setColor(Color.BLACK);
+        }
+
+        invalidate();
+    }
+
+    public void setStartLocation(float latitude, float longitude) {
+        if (startLat != 0 && startLong != 0)
+            return;
+
+        startLat = latitude;
+        startLong = longitude;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        prevScale = 0;
+    }
+
+    public void setLocation(float latitude, float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        newLoc = !newLoc;
+        invalidate();
+        prevScale = scale;
+    }
 }
